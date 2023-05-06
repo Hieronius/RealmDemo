f419c7298d13a9623b16b0e051d5819fac8d063a
Arsenty Halimovsky <ghost12@yandex.ru>
2023-05-06 15:37:16 +0300

    Create a new project

---
+++ RealmDemo/RealmDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmDemo
+//
+//  Created by Арсентий Халимовский on 06.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
