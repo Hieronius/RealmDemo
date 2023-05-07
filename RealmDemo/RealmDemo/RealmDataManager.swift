24a0bfa3c85d20436c5470e65f6edf0a4abe6178
Arsenty Halimovsky <ghost12@yandex.ru>
2023-05-07 17:57:56 +0300

    Add save(), load(), delete() methods

--- RealmDemo/RealmDemo/RealmDataManager.swift
+++ RealmDemo/RealmDemo/RealmDataManager.swift
@@ -12,27 +12,61 @@ protocol RealmDataManagerProtocol: AnyObject {
     
     static var shared: RealmDataManagerProtocol { get }
     
-    func save()
+    func save(object: Object)
     
-    func delete()
+    func delete(object: Object)
+    
+    func removeAllTasks(objects: [Object])
     
     func loadTasks()
+    
+    func loadTaskList()
 }
 
 class RealmDataManager: RealmDataManagerProtocol {
     
     static var shared: RealmDataManagerProtocol = RealmDataManager()
     
-    func save() {
-        // some code here
+    let realm = try! Realm()
+    
+    func save(object: Object) {
+        // begin work of the storage
+        realm.beginWrite()
+        // do some actions with storage
+        realm.add(object)
+        // end the work and save storage
+        // use "try!" because there can be no changes
+        try! realm.commitWrite()
     }
     
-    func delete() {
-        // some code here
+    func delete(object: Object) {
+        realm.beginWrite()
+        realm.delete(object)
+        try! realm.commitWrite()
+    }
+    
+    func removeAllTasks(objects: [Object]) {
+        realm.beginWrite()
+        realm.deleteAll()
+        try! realm.commitWrite()
     }
     
     func loadTasks() {
-        // some code here
+        for task in realm.objects(Task.self) {
+            print(task.taskName)
+            print(task.deadLine)
+            print("is it important - \(task.isImportant)")
+        }
+    }
+    
+    func loadTaskList() {
+        for list in realm.objects(TaskList.self) {
+            for task in list.taskList {
+                print(task.taskName)
+                print(task.deadLine)
+                print("is it important - \(task.isImportant)")
+            }
+        }
     }
     
 }
