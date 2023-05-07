ed2b906859d5d431bf674597841b46427105ff0d
Arsenty Halimovsky <ghost12@yandex.ru>
2023-05-07 17:00:31 +0300

    Add Realm save(), delete(), render()

--- RealmDemo/RealmDemo/ViewController.swift
+++ RealmDemo/RealmDemo/ViewController.swift
@@ -6,14 +6,61 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController {
+    
+    let realm = try! Realm()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view
+        realm.beginWrite()
+        delete()
+        try! realm.commitWrite()
+        
+        save()
+        
+        render()
+    }
+    
+    func delete() {
+        realm.delete(realm.objects(Person.self))
+    }
+    
+    func render() {
+        let people = realm.objects(Person.self)
+        for person in people {
+            let firstName = person.firstName
+            let secondName = person.lastName
+            let fullName = "\(firstName) \(secondName)"
+            
+            let label = UILabel(frame: view.bounds)
+            label.text = fullName
+            label.textAlignment = .center
+            label.numberOfLines = 0
+            view.addSubview(label)
+            label.font = UIFont(name: "Helvetica", size: 42)
+        }
+    }
+    
+    func save() {
+        let joe = Person()
+        joe.firstName = "Jenny"
+        joe.lastName = "Jhonson"
+        
+        realm.beginWrite()
+        realm.add(joe)
+        try! realm.commitWrite()
     }
 
 
+}
+
+class Person: Object {
+    @objc dynamic var firstName: String = ""
+    @objc dynamic var lastName: String = ""
+    @objc dynamic var age: Int = 0
+    
 }
 
